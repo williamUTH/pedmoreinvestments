1f0a407553c0e24781f1c44237260f4d0f80c71b
William Fisher <william@underthehammer.com>
2026-05-08 08:44:53 +0100

    Enable trailingSlash to fix direct URL access on Apache
    Without this, /portfolio direct URL hits return 404 on plain Apache
    hosts that don't have MultiViews enabled. Emitting /portfolio/index.html
    makes the directory + DirectorySlash mechanism work everywhere.

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@
 module.exports = {
   output: "export",
   images: { unoptimized: true }, // needed for next/image on static export
+  trailingSlash: true, // emit /portfolio/index.html instead of /portfolio.html for plain Apache hosts
   // basePath: '/subfolder' // only if hosting in a subfolder
 };
